4c0bc9496e15ce48b516594847507de9f9349da7
Rahulkand <88554473+Rahulkand@users.noreply.github.com>
2022-07-10 11:46:23 +0530

    Add files via upload

---
+++ imgage-slider.js
@@ -0,0 +1,13 @@
+let button_frt = document.querySelector(".btn-frnt"),
+    button_back = document.querySelector(".btn-back"),
+    button_frt_2 = document.querySelector(".btn-frnt-2"),
+    button_back_2 = document.querySelector(".btn-back-2"),
+    button_frt_3 = document.querySelector(".btn-frnt-3"),
+    button_back_3 = document.querySelector(".btn-back-3");
+    image_1 = document.querySelector(".Image"), 
+    image_2 = document.querySelector(".Image-2"),
+    image_3 = document.querySelector(".Image-3");
+    
+    button_frt.addEventListener("click",()=>{
+        image_1.classList.add("Image-js-2");
+    })
